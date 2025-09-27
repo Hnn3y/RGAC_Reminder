4c85b5c33c59c498e2e9e6758659d53f0c78792e
Enijeshiku <eenijeshiku@gmail.com>
2025-09-27 09:58:08 +0100

    .

--- index.js
+++ index.js
@@ -342,7 +342,4 @@ syncAndNotify().catch(err => {
   process.exit(1);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
